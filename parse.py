b4795defb46536690d9eb536d5eb7033b18e966d
cg021 <christiang021@gmail.com>
2020-12-18 22:48:38 -0600

    add parse script

---
+++ parse.py
@@ -0,0 +1,31 @@
+import requests
+import getopt
+import json
+import sys
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+       opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+       print 'test.py -i <inputfile> -o <outputfile>'
+       sys.exit(2)
+    for opt, arg in opts:
+       if opt in ("-i", "--ifile"):
+          inputfile = arg
+       if opt in ("-o", "--ofile"):
+          outputfile = arg
+
+    data = ''
+    with open(inputfile,'rb') as inf:
+        response = requests.post('https://jobs.lever.co/parseResume', files=dict(resume=inf))
+        data = response.json()
+        print(data)
+
+    with open(outputfile,'w') as outf:
+        json.dump(data, outf, indent=4)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
